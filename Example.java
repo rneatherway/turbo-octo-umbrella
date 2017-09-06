aa9055219270ece33a9bf9daab9c910b8f597cc3
Robin Neatherway <robin.neatherway@gmail.com>
2017-09-06 09:45:55 +0100

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,15 @@
+package com.something;
+
+class Example {
+    public static void Method(String a, String b) {
+        if (!true) {
+            throw new Exception("This happened: " + a
+                + " (instead of " + b + ")");
+        }
+    }
+    
+    public static void Method2() {
+        int x = 0;
+        String s = "hello";
+    }
+}
